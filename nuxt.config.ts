f0a9c990f5c9f2d835714a0530356d378356b05d
cuishunbiao <860026526@qq.com>
2023-09-12 03:25:01 +0800

    to: add title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,15 @@ export default defineNuxtConfig({
   css: [
     '@/assets/style/tailwind.css'
   ],
+  app: {
+    head: {
+      charset: 'utf-16',
+      title: 'pick color from image online',
+      meta: [
+        { name: 'description', content: 'pick color from image online' },
+        { name: 'keyworld', content: 'pick color from image, pick color from image online' }
+      ],
+    }
+  }
   // js: ['@/assets/js/picker.js']
 })
